cf69151707c77b2f194196643fb1a4b6be0af213
Arciesis <favier.mathis@gmail.com>
2024-02-29 16:06:21 +0100

    put lualine lazyness to false for better implementation

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,21 +5,6 @@ return {
    opts = {
       options = {
          theme = "molokai",
-         sections = {
-            lualine_c = {
-               "filename",
-               file_status = true,
-               newfile_status = false,
-               path = 1,
-               sorting_target = 40,
-               symbols = {
-                  modified = '[+]',      -- Text to show when the file is modified.
-                  readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-                  unnamed = '[No Name]', -- Text to show for unnamed buffers.
-                  newfile = '[New]',     -- Text to show for newly created file before first write
-               }
-            },
-         },
       },
    },
 }
