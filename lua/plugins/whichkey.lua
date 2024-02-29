2580348cef9633643917e4f9b8b0688f4b603586
Arciesis <favier.mathis@gmail.com>
2024-02-29 15:57:59 +0100

    Added harpoon and the needed keympas to whichKey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,28 +1,53 @@
 -- TODO: need to check for dual mappin between git and commented
+--  local harpoon = require("harpoon")
+-- basic telescope configuration
+--  local conf = require("telescope.config").values
+--  local function toggle_telescope(harpoon_files)
+    --  local file_paths = {}
+    --  for _, item in ipairs(harpoon_files.items) do
+        --  table.insert(file_paths, item.value)
+    --  end
+
+    --  require("telescope.pickers").new({}, {
+        --  prompt_title = "Harpoon",
+        --  finder = require("telescope.finders").new_table({
+            --  results = file_paths,
+        --  }),
+        --  previewer = conf.file_previewer({}),
+        --  sorter = conf.generic_sorter({}),
+    --  }):find()
+--  end
+
+
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = function()
-        local wk = require("which-key")
-        -- we will create the following mappings:
-        --  * <leader>ff find files
-        --  * <leader>fr show recent files
-        --  * <leader>fb Foobar
-        -- we'll document:
-        --  * <leader>fn new file
-        --  * <leader>fe edit file
-        -- and hide <leader>1
-        return wk.register({
-            ["1"] = "which_key_ignore",
-            -- File related
+   "folke/which-key.nvim",
+   dependencies = {
+      {"ThePrimeagen/harpoon", branch = "harpoon2"},
+      "nvim-telescope/telescope.nvim",
+   },
+   event = "VeryLazy",
+   opts = function()
+      local wk = require("which-key")
+      -- we will create the following mappings:
+      --  * <leader>ff find files
+      --  * <leader>fr show recent files
+      --  * <leader>fb Foobar
+      -- we'll document:
+      --  * <leader>fn new file
+      --  * <leader>fe edit file
+      -- and hide <leader>1
+      return wk.register({
+         ["1"] = "which_key_ignore",
+         -- File related
+         ["<leader>"] = {
             f = {
-                name = "File",
-                f = {"<cmd>Telescope find_files<CR>", "Find file"},
-                b = {"<cmd>Telescope buffers<CR>", "Buffers"},
-                c = {"<cmd>Telescope command_history last_sortused=true<CR>", "Command History"},
-                g = {"<cmd>Telescope git_files<cr>", "Find Files (git-files)" },
-                r = {"<cmd>Telescope oldfiles<CR>", "Recent" },
-                n = {"<cmd>enew<CR>", "New file"}
+               name = "File",
+               f = {"<cmd>Telescope find_files<CR>", "Find file"},
+               b = {"<cmd>Telescope buffers<CR>", "Buffers"},
+               c = {"<cmd>Telescope command_history last_sortused=true<CR>", "Command History"},
+               g = {"<cmd>Telescope git_files<cr>", "Find Files (git-files)" },
+               r = {"<cmd>Telescope oldfiles<CR>", "Recent" },
+               n = {"<cmd>enew<CR>", "New file"}
             },
 
             c = {
@@ -68,6 +93,13 @@ return {
                u = {"<cmd>DocsViewUpdate<CR>", "Update"},
             },
 
-        }, {prefix = "<leader>"})
-    end,
+            ["a"] = {function() require("harpoon"):list():append() end, "Add to harpoon", noremap=false},
+         },
+
+         ["<c-e>"] = {function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, "Toggle harpoon", noremap=true},
+         ["<c-p>"] = {function() require("harpoon"):list():prev()end, "Previous harpoon"},
+         ["<c-n>"] = {function() require("harpoon"):list():next()end, "Next harpoon"},
+         ["<c-d>"] = {function() require("harpoon"):list():clear()end, "Delete harpoon"},
+      })
+   end,
 }
