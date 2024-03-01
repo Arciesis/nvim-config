4ceda808f39fa99e6c3de3025ce3f3f331909a8e
Mathis Favier <favier.mathis@gmail.com>
2024-03-01 14:49:00 +0100

    Added some languages to Treesitter parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     opts = {
         ensure_installed = {
             "c",
+            "cpp",
             "lua",
             "vim",
             "vimcode",
             "query",
+            "rust",
+            "arduino",
+            "cmake",
+            "comment",
         },
         sync_install = true,
         auto_install = true,
