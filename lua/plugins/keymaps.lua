35f186bf528cb17ab8dc7a6c386154c236ef632d
Arciesis <favier.mathis@gmail.com>
2024-03-02 09:26:53 +0100

    Modified the keymaps to quit a terminal
    Now it's working (finally) c-q to terminate the terminal and c-w to hide it

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -158,13 +158,15 @@ vim.api.nvim_create_autocmd("TermOpen", {
    pattern = [[term://*]],
    callback = function(evt)
       local opts = { buffer = evt.buf }
-      vim.keymap.set('t', '<esc>', "exit<CR>", opts)
+      -- close the window and terminate the process
+      vim.keymap.set("t", "<c-q>", "<c-d>", opts)
       vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
       vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
       vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
       vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
       vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+      -- close the win without terminate the process (e.g: ou can return to the last point where you were)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w><c-w>]], opts)
    end
 
 })
