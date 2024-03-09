fa47a961ba15e374e22cb1c5a73e31f3844704f4
Mathis Favier <favier.mathis@gmail.com>
2024-03-09 19:07:55 +0100

    Readded mapping for comment, but not in the keymaps file

--- lua/plugins/commented.lua
+++ lua/plugins/commented.lua
@@ -1,11 +1,12 @@
 return {
     "winston0410/commented.nvim",
-    config = function() 
+    config = function()
        require("commented").setup({
           comment_padding = " ",
           prefer_block_comment = false,
           set_keybindings = true,
           ex_mode_cmd = "Comment",
+          keybindings = {nl = "gcc"}
        })
     end
 }
