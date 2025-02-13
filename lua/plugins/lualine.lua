c732f44a3b960b134975f7a2c223e934868b70e4
Mathis Favier <favier.mathis@gmail.com>
2025-02-13 16:52:42 +0100

    Fix the issue of lualine not showing up correctly (it was a prority issue with the color schem)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,8 @@
-local project_root = {
-  function()
-    return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-  end,
-  icon = "",
-  cond = hide_in_width,
-  separator = ' ',
-}
-
 return {
    'nvim-lualine/lualine.nvim',
    dependencies = { 'nvim-tree/nvim-web-devicons' },
    lazy = false,
+   priority = 1,
    config = function(_, _)
       require("lualine").setup({
          options = {
@@ -18,8 +10,8 @@ return {
          },
          sections = {
             lualine_c = {
-               project_root,
-               { "filename", file_status=true, newfile_status=true, path=1, },
+               -- project_root,
+               { "filename", file_status = true, newfile_status = true, path = 1 },
             },
          },
       })
