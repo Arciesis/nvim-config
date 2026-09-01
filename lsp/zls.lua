30bbdbf8ed42ba4f6ab870324e0bbff213a8ee91
Arciesis <8741388-Arciesis@users.noreply.gitlab.com>
2026-09-01 07:24:29 +0200

    zls config

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,3 +1,10 @@
 return {
-    cmd = { 'true' },
+    cmd = function(dispatchers, root_dir)
+        local marker = root_dir and (root_dir .. "/.zls-bin")
+        local path = "zls"
+        if marker and vim.fn.filereadable(marker) == 1 then
+            path = vim.fn.trim(vim.fn.readfile(marker)[1])
+        end
+        return vim.lsp.rpc.start(path, {}, dispatchers)
+    end
 }
